6b3b73da4fe7eadec75ede9bba46dd489a27099f
taebinkim7 <44902482+taebinkim7@users.noreply.github.com>
2020-10-30 00:17:59 -0400

    Update train_trans.py

--- train_trans.py
+++ train_trans.py
@@ -1,20 +1,28 @@
-  
-from argparse import ArgumentParser
-import tensorflow as tf
 import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 from os import makedirs
-import PIL
 from tensorflow.keras import layers
 import time
-from IPython import display
 
-parser = ArgumentParser(description='MCMC_GAN')
+from model import Encoder, Decoder, D_aae
+from loss import D_loss, G_loss, AE_loss
+
+from train_aae import encoder, decoder, d_aae, ae_optimizer, enc_optimizer, d_aae_optimizer
+
+parser = ArgumentParser(description='TRANS')
 parser.add_argument('--out_dir', type=str, action='store')
 args = parser.parse_args()
 
 makedirs(args.out_dir, exist_ok=True)
+
+aae_ckpt_dir = './aae_checkpoints'
+aae_ckpt_prefix = os.path.join(aae_ckpt_dir, "aae_ckpt")
+aae_checkpoint = tf.train.Checkpoint(ae_optimizer=ae_optimizer, enc_optimizer=enc_optimizer, d_aae_optimizer=d_aae_optimizer,
+                                     encoder=encoder, decoder=decoder, d_aae=d_aae)
+
+
+
 
 plot_images(no n_steps)
