05504524482b8db7908d5045e7266a65c192c579
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2020-11-05 15:03:10 -0500

    Update train_wbg.py

--- train_wbg.py
+++ train_wbg.py
@@ -10,8 +10,8 @@ import time
 from IPython import display
 
 from model import Encoder, Generator, Discriminator
-from loss import D_loss, G_loss
-from func import plot_images
+from loss import C_loss, EG_loss_wass
+from func import plot_images, gradient_penalty
 
 parser = ArgumentParser(description='bigan')
 parser.add_argument('--out_dir', type=str, action='store')
@@ -23,6 +23,7 @@ BATCH_SIZE = 64
 EPOCHS = 100
 LATENT_DIM = 128
 GP_WEIGHT = 10
+NUM_CRITIC = 5
 
 # SIGMA = 1.
 # GAMMA = 0.1
@@ -46,11 +47,11 @@ train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
 # Define models
 enc = Encoder(train_images[0].shape, LATENT_DIM)
 gen = Generator(train_images[0].shape, LATENT_DIM)
-disc = Discriminator(train_images[0].shape, LATENT_DIM)
+crit = Critic(train_images[0].shape, LATENT_DIM)
 
 # Define optimizers
-g_optimizer = tf.keras.optimizers.Adam(1e-4)
-d_optimizer = tf.keras.optimizers.Adam(1e-4)
+eg_optimizer = tf.keras.optimizers.Adam(1e-4)
+c_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 # Batch and shuffle the data
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
@@ -59,14 +60,14 @@ train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_
 # wbg_ckpt_dir = './wbg_checkpoints'
 wbg_ckpt_dir = os.path.join(args.out_dir, 'wbg_checkpoints')
 wbg_ckpt_prefix = os.path.join(wbg_ckpt_dir, 'wbg_ckpt')
-wbg_ckpt = tf.train.Checkpoint(g_optimizer=g_optimizer, d_optimizer=d_optimizer,
-                              enc=enc, gen=gen, disc=disc)
+wbg_ckpt = tf.train.Checkpoint(eg_optimizer=eg_optimizer, c_optimizer=c_optimizer,
+                              enc=enc, gen=gen, crit=crit)
 
 
 # Train steps
 @tf.function
-def train_step(batch_x):
-    with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
+def train_step_c(batch_x):
+    with tf.GradientTape() as eg_tape, tf.GradientTape() as c_tape:
         x = batch_x
         ex = enc(x, training=True)
         
@@ -76,26 +77,46 @@ def train_step(batch_x):
         x_ex = disc([x, ex], training=True)
         gz_z = disc([gz, z], training=True)
         
-        d_loss = D_loss(x_ex, gz_z)
-        g_loss = G_loss(x_ex, gz_z)
+        gp = gradient_penalty(partial(crit, training=True), x, ex, z, gz)
+        
+        c_loss = C_loss(x_ex, gz_z) + GP_WEIGHT * gp
 
-    g_gradient = g_tape.gradient(g_loss, enc.trainable_variables + gen.trainable_variables)
-    g_optimizer.apply_gradients(zip(g_gradient, enc.trainable_variables + gen.trainable_variables))
-    d_gradient = d_tape.gradient(d_loss, disc.trainable_variables)
-    d_optimizer.apply_gradients(zip(d_gradient, disc.trainable_variables))
+    eg_gradient = eg_tape.gradient(eg_loss, enc.trainable_variables + gen.trainable_variables)
+    eg_optimizer.apply_gradients(zip(eg_gradient, enc.trainable_variables + gen.trainable_variables))
+    c_gradient = c_tape.gradient(c_loss, crit.trainable_variables)
+    c_optimizer.apply_gradients(zip(c_gradient, crit.trainable_variables))
     
-    return g_loss, d_loss
+    return c_loss
 
+def train_step_eg(batch_x):
+    with tf.GradientTape() as eg_tape:
+        x = batch_x
+        
+        z = tf.random.normal([x.shape[0], LATENT_DIM])
+        gz = gen(z, training=True)
+        
+        eg_loss = EG_loss_wass(x_ex, gz_z)
+        
+    eg_gradient = eg_tape.gradient(eg_loss, enc.trainable_variables + gen.trainable_variables)
+    eg_optimizer.apply_gradients(zip(eg_gradient, enc.trainable_variables + gen.trainable_variables))
+    
+    return eg_loss
+  
 def train(dataset, n_epoch):    
     for epoch in range(n_epoch):
         start = time.time()
         
-        g_loss, d_loss = 0, 0
+        eg_loss, c_loss = 0, 0
         
         for batch in dataset:
-            g_loss_batch, d_loss_batch = train_step(batch)
-            g_loss += g_loss_batch
-            d_loss += d_loss_batch
+          
+            for _ in range(NUM_CRITIC):
+                c_loss_batch = train_step_c(batch)
+            
+            eg_loss_batch = train_step_eg(batch)
+            
+            eg_loss += eg_loss_batch
+            c_loss += c_loss_batch
         
         display.clear_output(wait=True)
         seed_images = train_images[0:NUM_EXAMPLES]
@@ -107,7 +128,7 @@ def train(dataset, n_epoch):
         plot_images(epoch + 1, fake_images[0:NUM_EXAMPLES], fake_images[NUM_EXAMPLES:2*NUM_EXAMPLES], args.out_dir, 'generate')
         
         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-        print ('G loss is {} and D loss is {}'.format(g_loss, d_loss))
+        print ('G loss is {} and D loss is {}'.format(eg_loss, c_loss))
 
 # Train
 train(train_dataset, EPOCHS)
