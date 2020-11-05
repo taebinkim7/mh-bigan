e128b3ca0e47786b794669fe56910a6b6c26d38a
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2020-11-05 12:11:10 -0500

    Update train_mhbg.py

--- train_mhbg.py
+++ train_mhbg.py
@@ -5,125 +5,112 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from os import makedirs
-from tensorflow.keras import layers
 import time
+from IPython import display
 
-from model import Transition, D_image, Encoder, Decoder, D_aae
-from loss import D_loss, G_loss, Trans_loss, mse
-from func import mh_update, plot_images
+from model import Encoder, Generator, Discriminator
+from loss import D_loss, G_loss
+from func import plot_images, mh_update
 
-parser = ArgumentParser(description='TRANS')
+parser = ArgumentParser(description='bigan')
 parser.add_argument('--out_dir', type=str, action='store')
 args = parser.parse_args()
 
-
 # Hyperparameters
-BUFFER_SIZE = 50000
-BATCH_SIZE = 128
+BUFFER_SIZE = 60000
+BATCH_SIZE = 64
 EPOCHS = 100
-LATENT_DIM = 200
-HIDDEN_DIM = 3000
+LATENT_DIM = 128
 
 SIGMA = 1.
 GAMMA = 0.1
 
 NUM_EXAMPLES = 20
-
-# Restore AAE
-encoder = Encoder(LATENT_DIM, HIDDEN_DIM)
-decoder = Decoder(LATENT_DIM, HIDDEN_DIM)
-d_aae = D_aae(LATENT_DIM, HIDDEN_DIM)
-
-ae_optimizer = tf.keras.optimizers.Adam(1e-4)
-enc_optimizer = tf.keras.optimizers.Adam(1e-4)
-d_aae_optimizer = tf.keras.optimizers.Adam(1e-4)
-
-aae_ckpt_dir = './aae_checkpoints'
-aae_ckpt_prefix = os.path.join(aae_ckpt_dir, "aae_ckpt")
-aae_checkpoint = tf.train.Checkpoint(ae_optimizer=ae_optimizer, enc_optimizer=enc_optimizer, d_aae_optimizer=d_aae_optimizer,
-                                     encoder=encoder, decoder=decoder, d_aae=d_aae)
-
-aae_checkpoint.restore(tf.train.latest_checkpoint(aae_ckpt_dir))
-
+NUM_CHANNELS = 1
 
 # Create a directory
 makedirs(args.out_dir, exist_ok=True)
 
-# Load data
-(train_images, train_labels), (_, _) = tf.keras.datasets.cifar10.load_data()
-train_images = train_images.astype('float32')
+# # Load data
+# (train_images, train_labels), (_, _) = tf.keras.datasets.cifar10.load_data()
+# train_images = train_images.astype('float32')
+# train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
+
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = np.pad(train_images, [(0,0), (2,2), (2,2)])
+train_images = train_images.reshape(train_images.shape[0], 32, 32, 1).astype('float32')
 train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
 
 # Define models
-transition = Transition(LATENT_DIM)
-d_image = D_image()
+enc = Encoder(train_images[0].shape, LATENT_DIM)
+gen = Generator(train_images[0].shape, LATENT_DIM)
+disc = Discriminator(train_images[0].shape, LATENT_DIM)
 
 # Define optimizers
-trans_optimizer = tf.keras.optimizers.Adam(1e-4)
-d_image_optimizer = tf.keras.optimizers.Adam(1e-4)
-
-# Load data
+g_optimizer = tf.keras.optimizers.Adam(1e-4)
+d_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 # Batch and shuffle the data
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-train_dataset1 = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
 # Checkpoint
-trans_ckpt_dir = './trans_checkpoints'
-trans_ckpt_prefix = os.path.join(trans_ckpt_dir, "trans_ckpt")
-trans_checkpoint = tf.train.Checkpoint(trans_optimizer=trans_optimizer, d_image_optimizer=d_image_optimizer,
-                                       transition=transition, d_image=d_image)
+# mhbg_ckpt_dir = './mhbg_checkpoints'
+mhbg_ckpt_dir = os.path.join(args.out_dir, 'mhbg_checkpoints')
+mhbg_ckpt_prefix = os.path.join(mhbg_ckpt_dir, 'mhbg_ckpt')
+mhbg_ckpt = tf.train.Checkpoint(g_optimizer=g_optimizer, d_optimizer=d_optimizer,
+                              enc=enc, gen=gen, disc=disc)
+
 
 # Train steps
 @tf.function
-def train_step_mc(batch_x, batch_x1):
-    with tf.GradientTape() as trans_tape, tf.GradientTape() as d_image_tape:
-
-        start_x = batch_x
-        start_z = encoder(start_x, training=False)
-        next_z = mh_update(start_z, GAMMA)
-        e = next_z - start_z
-        next_x = transition([start_x, e], training=True)
-        next_ex = encoder(next_x, training=False)
+def train_step(batch_x):
+    with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
+        x = batch_x
+        ex = enc(x, training=True)
         
-        # Image loss
-        real_out_image = d_image(batch_x1, training=True) # different shuffle
-        fake_out_image = d_image(next_x, training=True)
+        z = tf.random.normal([x.shape[0], LATENT_DIM])
+        gz = gen(z, training=True)
+        
+        ex1 = mh_update(ex, GAMMA)
+        x1 = gen(ex1, training=True)
+        
+        x_ex = disc([x, ex], training=True)
+        gz_z = disc([gz, z], training=True)
+        x1_ex1 = disc([x1, ex1], training=True)
+        
+        d_loss = D_loss(x_ex, gz_z) + D_loss(x_ex, x1_ex1)
+        g_loss = G_loss(x_ex, gz_z) + G_loss(x_ex, x1_ex1)
 
-        d_image_loss = D_loss(real_out_image, fake_out_image) 
-
-        # Trans loss
-        t_image_loss = G_loss(fake_out_image) 
-        t_code_loss = mse(next_z, next_ex)
-        trans_loss = t_image_loss + 100 * t_code_loss
+    g_gradient = g_tape.gradient(g_loss, enc.trainable_variables + gen.trainable_variables)
+    g_optimizer.apply_gradients(zip(g_gradient, enc.trainable_variables + gen.trainable_variables))
+    d_gradient = d_tape.gradient(d_loss, disc.trainable_variables)
+    d_optimizer.apply_gradients(zip(d_gradient, disc.trainable_variables))
     
-    trans_gradient = trans_tape.gradient(trans_loss, transition.trainable_variables)
-    trans_optimizer.apply_gradients(zip(trans_gradient, transition.trainable_variables))
-    d_image_gradient = d_image_tape.gradient(d_image_loss, d_image.trainable_variables)
-    d_image_optimizer.apply_gradients(zip(d_image_gradient, d_image.trainable_variables))
+    return g_loss, d_loss
 
-    return d_image_loss, trans_loss
-
-def train_mc(dataset, dataset1, n_epoch):
+def train(dataset, n_epoch):    
     for epoch in range(n_epoch):
-        tf.random.set_seed(10)
         start = time.time()
-
-        d_image_loss, trans_loss = 0, 0
-
-        for image_batch, image_batch1 in zip(dataset, dataset1):
-            d_image_loss_batch, trans_loss_batch = train_step_mc(image_batch, image_batch1)
-            d_image_loss += d_image_loss_batch 
-            trans_loss += trans_loss_batch
         
+        g_loss, d_loss = 0, 0
+        
+        for batch in dataset:
+            g_loss_batch, d_loss_batch = train_step(batch)
+            g_loss += g_loss_batch
+            d_loss += d_loss_batch
+        
+        display.clear_output(wait=True)
         seed_images = train_images[0:NUM_EXAMPLES]
-        e = GAMMA * tf.random.normal([NUM_EXAMPLES, LATENT_DIM])
-        next_images = transition([seed_images, e], training=False)
-        plot_images(epoch + 1, seed_images, next_images, args.out_dir)
-
-        print('D loss is {} and T loss is {}'.format(d_image_loss, trans_loss))
+        next_images = gen(enc(seed_images, training=False), training=False)
+        plot_images(epoch + 1, seed_images, next_images, args.out_dir, 'reconstruct')
+        
+        seed_codes = tf.random.normal([0:2*NUM_EXAMPLES, LATENT_DIM])
+        fake_images = gen(seed_codes, training=False)
+        plot_images(epoch + 1, fake_images[0:NUM_EXAMPLES], fake_images[NUM_EXAMPLES:2*NUM_EXAMPLES], args.out_dir, 'generate')
+        
         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+        print ('G loss is {} and D loss is {}'.format(g_loss, d_loss))
 
 # Train
-train_mc(train_dataset, train_dataset1, EPOCHS)
-trans_checkpoint.save(file_prefix = trans_ckpt_prefix)
+train(train_dataset, EPOCHS)
+mhbg_ckpt.save(file_prefix = mhbg_ckpt_prefix)
