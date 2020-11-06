40b9c57df1318248c6d7f4947b3d3059ba89e82e
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2020-11-05 21:54:06 -0500

    Update model.py

--- model.py
+++ model.py
@@ -66,14 +66,12 @@ def Discriminator(img_dim, lat_dim):
     x = layers.Flatten()(x)
 
     z = layers.Flatten()(inputs_z)
-    z = layers.Dense(512, kernel_initializer='he_normal')(z)
-    z = layers.Dropout(0.2)(z)
-    z = layers.Dense(512, kernel_initializer='he_normal')(z)
+    z = layers.Dense(512)(z)
     z = layers.Dropout(0.2)(z)
 
     xz = layers.concatenate([x, z])
 
-    outputs = layers.Dense(1, kernel_initializer='he_normal')(xz)
+    outputs = layers.Dense(1)(xz)
     model = tf.keras.Model(inputs=[inputs_x, inputs_z], outputs=outputs)
     return model
 
@@ -81,13 +79,13 @@ def Critic(img_dim, lat_dim):
     inputs_x = tf.keras.Input(shape=img_dim)
     inputs_z = tf.keras.Input(shape=(lat_dim,))
 
-    x = layers.Conv2D(64, (5, 5), (2, 2), padding='same', kernel_initializer='he_normal')(inputs_x)
+    x = layers.Conv2D(128, (5, 5), (2, 2), padding='same', kernel_initializer='he_normal')(inputs_x)
     # x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
-    x = layers.Conv2D(128, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+    x = layers.Conv2D(256, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
 #     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
-    x = layers.Conv2D(256, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+    x = layers.Conv2D(512, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
 #     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
     x = layers.Dropout(0.2)(x)
@@ -98,11 +96,13 @@ def Critic(img_dim, lat_dim):
     x = layers.Flatten()(x)
 
     z = layers.Flatten()(inputs_z)
-    z = layers.Dense(512, kernel_initializer='he_normal')(z)
+    z = layers.Dense(512)(z)
     z = layers.Dropout(0.2)(z)
 
     xz = layers.concatenate([x, z])
+    xz = layers.Dense(1024)(xz)
+    xz = layers.LeakyReLU()(xz)
 
-    outputs = layers.Dense(1, kernel_initializer='he_normal')(xz)
+    outputs = layers.Dense(1)(xz)
     model = tf.keras.Model(inputs=[inputs_x, inputs_z], outputs=outputs)
     return model
