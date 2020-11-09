de58682762e41d9b54651799264c3b63ead69f05
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2020-11-08 23:33:36 -0500

    Update model.py

--- model.py
+++ model.py
@@ -10,9 +10,9 @@ def Encoder(img_dim, lat_dim):
     x = layers.Conv2D(256, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
-    x = layers.Conv2D(512, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+#     x = layers.Conv2D(512, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+#     x = layers.BatchNormalization()(x)
+#     x = layers.ReLU()(x)
     x = layers.Conv2D(512, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
@@ -23,7 +23,7 @@ def Encoder(img_dim, lat_dim):
     return model
 
 def Generator(img_dim, lat_dim):
-    dim16 = img_dim[0] // 16
+    dim16 = img_dim[0] // 8
 
     inputs = tf.keras.Input(shape=(lat_dim,))
 
@@ -31,9 +31,9 @@ def Generator(img_dim, lat_dim):
     x = layers.Reshape([dim16, dim16, 512])(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
-    x = layers.Conv2DTranspose(512, (5, 5), (2, 2), padding='same', use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+#     x = layers.Conv2DTranspose(512, (5, 5), (2, 2), padding='same', use_bias=False)(x)
+#     x = layers.BatchNormalization()(x)
+#     x = layers.ReLU()(x)
     x = layers.Conv2DTranspose(256, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
@@ -80,8 +80,13 @@ def Discriminator(img_dim, lat_dim):
 def Critic(img_dim, lat_dim):
     inputs_x = tf.keras.Input(shape=img_dim)
     inputs_z = tf.keras.Input(shape=(lat_dim,))
+    
+    z = layers.RepeatVector(1024)(inputs_z)
+    z = layers.Reshape([32, 32, lat_dim])(z)
+    
+    x = layers.concatenate([inputs_x, z])
 
-    x = layers.Conv2D(128, (5, 5), (2, 2), padding='same', kernel_initializer='he_normal')(inputs_x)
+    x = layers.Conv2D(128, (5, 5), (2, 2), padding='same', kernel_initializer='he_normal')(x)
     # x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
     x = layers.Conv2D(256, (5, 5), (2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
@@ -97,14 +102,6 @@ def Critic(img_dim, lat_dim):
     x = layers.Dropout(0.2)(x)
     x = layers.Flatten()(x)
 
-    z = layers.Flatten()(inputs_z)
-    z = layers.Dense(512)(z)
-    z = layers.Dropout(0.2)(z)
-
-    xz = layers.concatenate([x, z])
-    xz = layers.Dense(1024)(xz)
-    xz = layers.LeakyReLU()(xz)
-
-    outputs = layers.Dense(1)(xz)
+    outputs = layers.Dense(1)(x)
     model = tf.keras.Model(inputs=[inputs_x, inputs_z], outputs=outputs)
     return model
